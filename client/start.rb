1be582dde5c7f8bca0f071c5c69f69701f2643c3
RadoRado <radorado@hackbulgaria.com>
2015-08-28 11:12:16 +0300

    Remove HTTP calls from start.rb

--- client/start.rb
+++ client/start.rb
@@ -2,23 +2,17 @@ require 'json'
 require 'http'
 require 'yaml'
 
+require_relative 'state'
 require_relative 'view_dispatcher'
 
 module DonnaClient
   class << self
     def start
       configuration = YAML::load_file(File.join(__dir__, 'config.yml'))
-      vd = ViewDispatcher.new :home, configuration
-      vd.dispatch
-      # payload = {
-      #   "email" => "radorado@hackbulgaria.com",
-      #   "name" => "RadoRado",
-      #   "password" => "123ruby"
-      # }
+      DonnaClient::State.conf = configuration
 
-      # r = HTTP.post(configuration["api_url"] + '/user/register', :json => payload)
-      # p r.status
-      # p JSON.parse(r.body.to_s)
+      vd = ViewDispatcher.new :home
+      vd.dispatch
     end
   end
 end
