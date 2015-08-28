c00a713b8ebb403357f5d85456dae80b7c91e251
RadoRado <radorado@hackbulgaria.com>
2015-08-28 11:33:04 +0300

    Client login now works properly

--- client/controllers/user.rb
+++ client/controllers/user.rb
@@ -13,11 +13,18 @@ module DonnaClient
         r = HTTP.post(DonnaClient::State["api_url"] + '/user/register', :json => payload)
         p r.status
         p JSON.parse(r.body.to_s)
-        # Save state?
       end
 
       def login(email, password)
+        payload = {
+          "email" => email,
+          "password" => password
+        }
 
+        r = HTTP.post(DonnaClient::State["api_url"] + '/user/login', :json => payload)
+        body = JSON.parse(r.body.to_s)
+        p body
+        return r.status == 200
       end
     end
   end
---
+++ client/views/login.rb
@@ -0,0 +1,36 @@
+module DonnaClient
+  module Views
+    class Login
+      def take_control
+        loop do
+          puts render
+
+          puts "Your email:"
+          email = STDIN.gets
+
+          puts "Your password:"
+          password = STDIN.gets
+
+          email, password = [email, password].map(&:strip)
+
+          if DonnaClient::Controllers::User.login email, password
+            sleep 5
+            return :profile
+          end
+
+          puts "Wrong username/password! Try again"
+        end
+      end
+
+      def render
+        content = <<-CONTENT
+          In order to login, please provide email ans password:
+        CONTENT
+
+        DonnaClient::Views.trim_lines content
+      end
+    end
+  end
+end
+
+DonnaClient::Views.register_view :login, DonnaClient::Views::Login
