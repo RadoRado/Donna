34f170e1d2935c9b5492e1e6ff98dfda3cc174c2
RadoRado <radorado@hackbulgaria.com>
2015-08-28 15:38:21 +0300

    Load settings from local_settings.yml

--- app.rb
+++ app.rb
@@ -1,11 +1,14 @@
 require 'sinatra/base'
-require "sinatra/activerecord"
+require 'sinatra/config_file'
+require 'sinatra/activerecord'
 
 require_relative 'models/init'
 require_relative 'routes/init'
 
 class Donna < Sinatra::Base
-  set :app_file, __FILE__
-
   register Sinatra::ActiveRecordExtension
+  register Sinatra::ConfigFile
+
+  config_file 'config/local_settings.yml'
+  set :app_file, __FILE__
 end
