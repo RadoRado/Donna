334c884348b7da5a5ad9afd5e04d3f21e644c722
RadoRado <radorado@hackbulgaria.com>
2015-09-02 18:17:06 +0300

    Clean up user tests & test basic get contact

--- tests/routes/user_test.rb
+++ tests/routes/user_test.rb
@@ -11,6 +11,12 @@ class UserRoutesTest < Minitest::Test
 
   def setup
     DatabaseCleaner.start
+    user = User.create(email: 'always@registered.com',
+                       name: 'Rado the Registered',
+                       password: '123')
+    user.save
+
+    @always_registered = user
   end
 
   def teardown
@@ -33,19 +39,19 @@ class UserRoutesTest < Minitest::Test
   end
 
   def test_register_with_missing_fields
-    payload = {"name" => "Rado" }.to_json
+    payload = { 'name' => 'Rado' }.to_json
 
     post '/user/register', payload
     body = JSON.parse(last_response.body)
 
     assert_equal 400, last_response.status
-    assert body.key? "message"
+    assert body.key? 'message'
   end
 
   def test_register_successfully
-    payload = {"name" => "Rado",
-               "email" => "radorado@hackbulgaria.com",
-               "password" => "mylittlesecret"}.to_json
+    payload = { 'name' => 'Rado',
+                'email' => 'radorado@hackbulgaria.com',
+                'password' => 'mylittlesecret' }.to_json
 
     post '/user/register', payload
     body = JSON.parse(last_response.body)
@@ -55,9 +61,9 @@ class UserRoutesTest < Minitest::Test
   end
 
   def test_register_existing_user
-    payload = {"name" => "Rado",
-               "email" => "radorado@hackbulgaria.com",
-               "password" => "mylittlesecret"}.to_json
+    payload = { 'name' => 'Rado',
+                'email' => 'radorado@hackbulgaria.com',
+                'password' => 'mylittlesecret' }.to_json
 
     post '/user/register', payload
     post '/user/register', payload
@@ -68,23 +74,23 @@ class UserRoutesTest < Minitest::Test
   end
 
   def test_login_with_missing_email
-    payload = { "password" => "mylittlesecret"}.to_json
+    payload = { 'password' => 'mylittlesecret' }.to_json
     post '/user/login', payload
 
     assert_equal 400, last_response.status
   end
 
   def test_login_with_missing_password
-    payload = { "email" => "radorado@hackbulgaria.com"}.to_json
+    payload = { 'email' => 'radorado@hackbulgaria.com' }.to_json
     post '/user/login', payload
 
     assert_equal 400, last_response.status
   end
 
   def test_user_login_successfuly
-    payload = {"email" => "radorado@hackbulgaria.com",
-               "name" => "Rado",
-               "password" => "mylittlesecret"}.to_json
+    payload = { 'email' => 'radorado@hackbulgaria.com',
+                'name' => 'Rado',
+                'password' => 'mylittlesecret' }.to_json
 
     post '/user/register', payload
     post '/user/login', payload
@@ -93,9 +99,9 @@ class UserRoutesTest < Minitest::Test
   end
 
   def test_user_login_with_wrong_password
-    payload = {"name" => "Rado",
-               "email" => "radorado@hackbulgaria.com",
-               "password" => "mylittlesecret"}.to_json
+    payload = { 'name' => 'Rado',
+                'email' => 'radorado@hackbulgaria.com',
+                'password' => 'mylittlesecret' }.to_json
 
     post '/user/register', payload
 
@@ -116,5 +122,11 @@ class UserRoutesTest < Minitest::Test
 
     post '/user/login', login_payload
     assert_equal 404, last_response.status
+  end
+
+  def test_get_contact_for_existing_user
+    get '/user/contact/' + @always_registered.id.to_s
+
+    assert_equal 200, last_response.status
   end
 end
